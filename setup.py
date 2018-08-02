785c2ff1cc1037bbb7bf86503f2ccddb84e4f76d
Ubuntu <caozon@code-dev.4co5vkhwnyru5da0oj2ltjzlib.hx.internal.cloudapp.net>
2018-08-02 01:35:10 +0000

    improve the setup.py
    Signed-off-by: Ubuntu <caozon@code-dev.4co5vkhwnyru5da0oj2ltjzlib.hx.internal.cloudapp.net>

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='deep_mapper',
-      version='0.1.2',
-      description='Mapper that able to return an object with converted keys, or a list of objects',
-      url='http://github.com/gebriallairbeg/deep_mapper',
-      author='Pavel Sherepo',
-      author_email='pavel.sherepo@gmail.com',
+setup(name='deep_mapper1',
+      version='0.0.1',
+      description='This is a fork of gebriallairbeg/deep_mapper. It is able to return an object with converted keys, or a list of objects',
+      url='http://github.com/cao5zy/deep_mapper',
+      author='zongying.cao',
+      author_email='zongying_cao@163.com',
       license='MIT',
       packages=['deep_mapper'],
       zip_safe=False)
